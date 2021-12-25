e9500ee82989e3b9bfac5e6ea91989727bacf968
Tser9 <75836960+Tser9@users.noreply.github.com>
2021-12-25 12:58:45 +0300

    backfolder

---
+++ back/qwe/database/seeders/CartTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cart;
+use Illuminate\Database\Seeder;
+
+class CartTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $faker = \Faker\Factory::create();
+
+        // Create 50 product records
+        for ($i = 0; $i < 50; $i++) {
+            Cart::create([
+                'title' => $faker->title,
+                'description' => $faker->paragraph,
+                'price' => $faker->randomNumber(2),
+                'availability' => $faker->boolean(50)
+            ]);
+        }
+    }
+}
---
+++ back/qwe/routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('products', 'ProductsController@index');
+
+Route::get('products/{product}', 'ProductsController@show');
+
+Route::post('products','ProductsController@store');
+
+Route::put('products/{product}','ProductsController@update');
+
+Route::delete('products/{product}', 'ProductsController@delete');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('cart', 'CartController@index');
+
+Route::get('cart/{cart}', 'CartController@show');
+
+Route::post('cart','CartController@store');
+
+Route::put('cart/{cart}','CartController@update');
+
+Route::delete('cart/{cart}', 'CartController@delete');
+Route::delete('cart', 'CartController@destroy');
