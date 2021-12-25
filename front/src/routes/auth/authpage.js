06899373524ba22beb7384cafbf4ec910c54f7a0
Tser9 <75836960+Tser9@users.noreply.github.com>
2021-12-25 14:57:59 +0300

    oauth2

---
+++ front/src/routes/auth/authpage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ContextData from "../../context/data/contextData";
+import classes from "./../../style.module.css";
+import {useAuth0} from "@auth0/auth0-react";
+
+const Auth=()=>{
+
+    const{loginWithPopup,loginWithRedirect, logout,user, isAuthenticated}=useAuth0()
+
+
+    return(
+
+        <div className={classes.test1}>
+            <div> qwesd</div>
+            <button onClick={loginWithRedirect}> CLICK </button>
+            <button onClick={logout}> exit </button>
+            <h3>user is {isAuthenticated ? 'logged':'notloggoed'}</h3>
+            {isAuthenticated &&(
+                <pre style={{textAlign:'start'}}>
+                    {JSON.stringify(user,null,2)}
+                    {user.name}
+                </pre>
+
+
+            )}
+        </div>
+
+    )
+
+}
+export default Auth
