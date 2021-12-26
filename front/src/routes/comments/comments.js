4485082fb40c74e8a29434e5d80db1bdd3f05f5a
poklaad <danya_goncharuk01@mail.ru>
2021-12-26 14:17:01 +0300

    Update comments.js

--- front/src/routes/comments/comments.js
+++ front/src/routes/comments/comments.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from "../../style.module.css";
 import Navbar from "../../modules/navigation/nvabar";
 import Header from "../../modules/navigation/header";
@@ -12,12 +12,25 @@ const Comments=()=>{
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const {stateDataThree, dispatchDataThree} = React.useContext(ContextData)
     const comments=stateDataThree.comments;
+    const [value,setValue] =useState()
+    const [value1,setValue1] =useState()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://127.0.0.1:8000/api/comments',{
+            method: 'POST',
+            headers:{"Content-Type":"application/json"},
+            body: JSON.stringify({"comment_id": '',"fio":value,"feedback":value1})
 
+        }).then(()=>{
+        })
+
+
+    }
     // eslint-disable-next-line react-hooks/rules-of-hooks
     React.useEffect(()=>{
         const fetchShop= async ()=>{
             try{
-                const response=await fetch('http://127.0.0.1:8000/api/cart')
+                const response=await fetch('http://127.0.0.1:8000/api/comments')
                 if (response.status===200){
                     const result= await   response.json()
                     dispatchDataThree({
@@ -36,6 +49,7 @@ const Comments=()=>{
 
     return(
         <div>
+
         <div className={classes.main}>
         <div className={classes.navigation}>
         <Navbar />
@@ -53,10 +67,22 @@ const Comments=()=>{
                 }
 
             )}
-        <Link  className={classes.buy} to={"/buying"} > Покупка</Link>
-        </div>
+        <div className={classes.content}>
+        <form className={classes.Forms}>
+        <dev style={{marginTop: "10px"}}>
+    Автор:
+        <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
+    </dev>
+    <dev style={{marginTop: "10px"}}>
+    Комментарий:
+        <input type="text" value1={value1} onChange={event => setValue1(event.target.value)}/>
+    </dev>
+    </form>
+    <div className={classes.button2} onClick={handleSubmit}> Отправить комментарий</div>
+    </div>
+    </div>
 
-        </div></div>
+    </div></div>
 )
 
 }
